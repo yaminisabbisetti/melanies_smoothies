9adbfb507da7f87659059554cdb85c82720d33ae
Sabbisetti Lakshmi Yamini <31930071+yaminisabbisetti@users.noreply.github.com>
2025-10-01 11:46:24 -0700

    Added Requests Code

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,3 +37,8 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success("Your Smoothie is ordered, "+name_on_order+"!", icon="✅")
+
+# New section to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefront.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
